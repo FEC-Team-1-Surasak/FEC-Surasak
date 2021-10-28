186db2711bffa6603301d6b45bf50ac072e4a08c
Xiaohan Liu <danae0730@gmail.com>
2021-10-28 16:03:00 -0700

    get request to get questions based on product_id

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
@@ -21,6 +23,23 @@ app.get('/products', (req, res) => {
     })
     .catch((err) => {
       console.log(err);
+    });
+});
+
+app.get('/qa/questions', (req, res) => {
+  const options = {
+    headers: {
+      Authorization: config.API_KEY,
+    },
+  };
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions?product_id='+ req.query.product_id, options)
+    .then((response) => {
+      console.log('data is', response.data);
+      res.status(200).json(response.data);
+    })
+    .catch((err) => {
+      console.log('err');
+      res.status(500);
     });
 });
 
