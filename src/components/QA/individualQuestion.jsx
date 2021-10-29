43bf02c7fcb14c6288c78816e74314e8da99ea15
Xiaohan Liu <danae0730@gmail.com>
2021-10-29 07:31:02 -0700

    add click function to the individual question component

--- src/components/QA/individualQuestion.jsx
+++ src/components/QA/individualQuestion.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/extensions */
@@ -6,25 +10,42 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/jsx-no-undef */
 import React from 'react';
+import axios from 'axios';
 import AddAnswer from './AddAnswer.jsx';
 
 class IndividualQuestion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      helpfulness: 0,
+      helpfulness: this.props.question.question_helpfulness,
     };
+    this.onClick = this.onClick.bind(this);
   }
   // get helpfulness information form api '/qa/questions'
   // data.results.question_helpfulness
 
+  // click functions to update the counts of helpfulness
+  onClick() {
+    const count = this.state.helpfulness;
+    this.setState({ helpfulness: count + 1 });
+    // send a put
+    axios.put('/qa/questions/:question_id/helpful', { question_id: this.props.question.question_id })
+      .then(() => { console.log('Updating the record'); })
+      .catch((err) => { console.log('Error'); });
+  }
+
   render() {
     return (
       <div className="question">
-        <div>
-        <b> Q:</b>
-        {this.props.question.question_body}
-        Helpful?
+        <div className="question-body">
+          <b> Q:</b>
+          {this.props.question.question_body}
+          <span>Helpful?</span>
+          <span onClick={this.onClick}>
+            Yes(
+            {this.state.helpfulness}
+            )
+          </span>
         </div>
         {/* <AddAnswer /> */}
       </div>
