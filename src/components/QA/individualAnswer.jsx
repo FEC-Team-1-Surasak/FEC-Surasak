5f4610f897f9fe4165a51ca5bb0b452cb2f91879
Xiaohan Liu <danae0730@gmail.com>
2021-10-30 09:08:10 -0700

    create an indiviudal answer component to handle "helpfulness" functionality

---
+++ src/components/QA/individualAnswer.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable no-alert */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import axios from 'axios';
+
+class IndividualAnswer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cnt: 0,
+      helpfulness: this.props.answer.helpfulness,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    if (this.state.cnt === 1) {
+      alert('You have already marked this questions helfpful');
+    } else {
+      this.setState({ helpfulness: this.state.helpfulness + 1 });
+      this.setState({ cnt: 1 });
+      // send a put
+      axios.put('/qa/answers/:answer_id/helpful', { answer_id: this.props.answer.answer_id })
+        .then(() => {
+          console.log('UPDATED THE RECORD');
+        })
+        .catch((err) => { console.error(err); });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="answer">
+          {' '}
+          <b>A:</b>
+          {this.props.answer.body}
+        </div>
+        <div className="user-name">
+          by user
+          {this.props.answerer_name === 'Seller' ? <b>Seller</b> : this.props.answer.answer_name}
+          ,
+          {' '}
+          {new Date(this.props.date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}
+          <span>Helpful?</span>
+          <span onClick={this.onClick}>
+            Yes(
+            {this.state.helpfulness}
+            )
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default IndividualAnswer;
