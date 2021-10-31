2585d1c2316947d6d3a24815976fdcc8ac99d5b2
Xiaohan Liu <danae0730@gmail.com>
2021-10-31 16:22:49 -0700

    add search function

--- src/components/QA/questions.jsx
+++ src/components/QA/questions.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable array-callback-return */
+/* eslint-disable react/sort-comp */
+/* eslint-disable no-undef */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable func-call-spacing */
 /* eslint-disable max-len */
@@ -22,9 +26,13 @@ class Question extends React.Component {
       id: '37366',
       list: [],
       len: 2,
+      filteredList: [],
+      term: '',
 
     };
     this.getMoreQuestions = this.getMoreQuestions.bind(this);
+    this.onChange = this.onChange.bind(this);
+    // this.search = this.search.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +45,7 @@ class Question extends React.Component {
       .then(
         (response) => {
           this.setState({ list: response.data.results });
+          this.setState({ filteredList: response.data.results });
         },
       )
       .catch(
@@ -48,11 +57,31 @@ class Question extends React.Component {
     this.setState({ len: this.state.len + 2 });
   }
 
+  onChange(e) {
+    this.setState({ term: e.target.value }, () => {
+      if (this.state.term.length >= 3) {
+        const filtered = this.state.list.filter(
+          (question) => question.question_body.toLowerCase().includes(this.state.term.toLowerCase()),
+        );
+      //  filtered.map((list) => {
+      //     const item = list.question_body.toLowerCase();
+
+      //  });
+
+
+        this.setState({ filteredList: filtered });
+      } else {
+        this.setState({ filteredList: this.state.list });
+      }
+    });
+  }
+
   render() {
     return (
       <div>
+        <input placeholder="Have a question? Search for answers…" onChange={this.onChange} />
         <div className="question-list">
-          {this.state.list.slice(0, this.state.len).map((question) => <IndividualQuestion question={question} key={question.question_id} />)}
+          {this.state.filteredList.slice(0, this.state.len).map((question) => <IndividualQuestion question={question} key={question.question_id} term={this.state.term} />)}
         </div>
         {(this.state.list.length > 2 && this.state.len < this.state.list.length) ? <button className="add-quetsions" onClick={this.getMoreQuestions} type="submit"> MORE ANSWERED QUESTIONS</button> : ''}
       </div>
