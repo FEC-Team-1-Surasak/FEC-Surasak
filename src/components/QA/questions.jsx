94bb11ac1c24991896035226a984003ca13d6461
Xiaohan Liu <danae0730@gmail.com>
2021-11-02 14:09:08 -0700

    add state/functions to handle modal

--- src/components/QA/questions.jsx
+++ src/components/QA/questions.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-param-reassign */
 /* eslint-disable array-callback-return */
 /* eslint-disable react/sort-comp */
@@ -15,7 +17,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
 import IndividualQuestion from './individualQuestion.jsx';
+import Modal from './Modal.jsx';
 
 class Question extends React.Component {
   constructor(props) {
@@ -23,15 +27,19 @@ class Question extends React.Component {
 
     this.state = {
       // will change id to this.props.id once the component is ready to be deployed
-      id: '37366',
+      id: this.props.id,
       list: [],
       len: 2,
       filteredList: [],
       term: '',
+      model: false,
 
     };
     this.getMoreQuestions = this.getMoreQuestions.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.showModel = this.showModel.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+
     // this.search = this.search.bind(this);
   }
 
@@ -71,6 +79,14 @@ class Question extends React.Component {
     });
   }
 
+  showModel() {
+    this.setState({ modal: true });
+  }
+
+  closeModal() {
+    this.setState({ modal: false });
+  }
+
   render() {
     return (
       <div>
@@ -78,7 +94,10 @@ class Question extends React.Component {
         <div className="question-list">
           {this.state.filteredList.slice(0, this.state.len).map((question) => <IndividualQuestion question={question} key={question.question_id} term={this.state.term} />)}
         </div>
-        {(this.state.list.length > 2 && this.state.len < this.state.list.length) ? <button className="add-quetsions" onClick={this.getMoreQuestions} type="submit"> MORE ANSWERED QUESTIONS</button> : ''}
+        {(this.state.list.length > 2 && this.state.len < this.state.list.length) ? <button className="load-quetsions" onClick={this.getMoreQuestions} type="submit"> MORE ANSWERED QUESTIONS</button> : ''}
+        <button onClick={this.showModel}>ADD A QUESTION +</button>
+        {this.state.modal ? <Modal close={this.closeModal} productname={this.props.name} productid={this.props.id} /> : null}
+
       </div>
     );
   }
