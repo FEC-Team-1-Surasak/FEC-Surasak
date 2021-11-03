a43e5ea0468b1eb2c2620dd64c9ff3f986eeb3e9
Anna Yan <annayan102@gmail.com>
2021-11-03 11:37:40 -0400

    Display 'please select size' over size dropdown if user attempts to add item to cart without selected size

--- src/components/Overview/AddToCart.jsx
+++ src/components/Overview/AddToCart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/sort-comp */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable react/button-has-type */
 /* eslint-disable arrow-body-style */
@@ -12,20 +13,65 @@ class AddToCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // currentStyle: this.props.currentStyle,
       sku: '',
       size: '',
-      quantity: '',
+      quantity: null,
       bagQty: 1,
+      selectSize: true,
+      inStock: true,
     };
+
+    // this.updateStyle = this.updateStyle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.outOfStock = this.outOfStock.bind(this);
   }
 
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.currentStyle !== this.props.currentStyle) {
+  //     this.updateStyle(this.props.currentStyle);
+  //   }
+  // }
+
+  // updateStyle(style) {
+  //   console.log(style);
+  //   this.setState({
+  //     currentStyle: style,
+  //   });
+  // }
+
   handleClick() {
+    if (this.state.size) {
+      this.setState({
+        selectSize: true,
+      });
+    } else {
+      this.setState({
+        selectSize: false,
+      });
+    }
     this.props.addToCart({
       name: this.props.product.name,
       style: this.props.currentStyle.name,
       size: this.state.size,
       qty: this.state.bagQty,
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      sku: e.target.value,
+      quantity: this.props.currentStyle.skus[e.target.value].quantity,
+      size: this.props.currentStyle.skus[e.target.value].size,
+      selectSize: true,
+      inStock: this.props.currentStyle.skus[e.target.value].quantity !== 0,
+    });
+  }
+
+  outOfStock() {
+    this.setState({
+      inStock: false,
     });
   }
 
@@ -47,27 +93,30 @@ class AddToCart extends React.Component {
 
     return (
       <>
-        <select onChange={(e) => { this.setState({ sku: e.target.value, quantity: this.props.currentStyle.skus[e.target.value].quantity, size: this.props.currentStyle.skus[e.target.value].size }); }} >
+        <div>{this.state.selectSize ? '' : 'Please select size'}</div>
+        <select onChange={this.handleChange}>
           <option>Select Size</option>
           {Object.keys(this.props.currentStyle.skus).map(sku => {
             const size = this.props.currentStyle.skus[sku].size;
             const qty = this.props.currentStyle.skus[sku].quantity;
             if (qty !== 0) {
-              return <option value={sku}>{size}</option>;
+              return <option key={size} value={sku}>{size}</option>;
             }
           })}
         </select>
         {' '}
-        <select onChange={(e) => { this.setState({ bagQty: e.target.value }); }}>
+        <select
+          onChange={(e) => { this.setState({ bagQty: e.target.value }); }}
+        >
           <option>{this.state.size === '' ? '-' : 1}</option>
           {qtyCount.map(digit => {
             return <option value={digit}>{digit}</option>;
           })}
         </select>
         <br />
-        <button onClick={this.handleClick}>
-          Add to Cart
-        </button>
+        <div>
+          {this.state.inStock ? <button onClick={this.handleClick}>Add to Cart</button> : null}
+        </div>
       </>
     );
   }
