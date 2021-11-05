cc0596bf445d4a75920fc5b6a6879188ced75adc
Anna Yan <annayan102@gmail.com>
2021-11-04 21:05:54 -0400

    Add arrow styling to css file

--- src/components/Overview/Carousel/Arrows.jsx
+++ src/components/Overview/Carousel/Arrows.jsx
@@ -3,38 +3,16 @@
 import React from 'react';
 
 const Arrows = (props) => {
-  const arrowStyleRight = {
-    position: 'absolute',
-    'z-index': 10,
-    right: '20px',
-    top: '225px',
-    color: '#FFFAFA',
-    'font-weight': 'bold',
-    'text-shadow': 'black 0.1em 0.1em 0.2em',
-    'font-size': '18px',
-  };
-
-  const arrowStyleLeft = {
-    position: 'absolute',
-    'z-index': 10,
-    left: '100px',
-    top: '225px',
-    color: '#FFFAFA',
-    'font-weight': 'bold',
-    'text-shadow': 'black 0.1em 0.1em 0.2em',
-    'font-size': '18px',
-  };
-
   if (props.direction === 'right') {
     return (
-      <div style={arrowStyleRight} onClick={props.next}>
+      <div className="overview-carousel-arrow-R" onClick={props.next}>
         {'>'}
       </div>
     );
   }
   if (props.direction === 'left') {
     return (
-      <div style={arrowStyleLeft} onClick={props.previous}>
+      <div className="overview-carousel-arrow-L" onClick={props.previous}>
         {'<'}
       </div>
     );
