f5b15e81b17c94f398b255a06bfd5866c133d46d
Anna Yan <annayan102@gmail.com>
2021-11-06 20:50:11 -0400

    Expanded image zooms in 2.5x on mouse click

--- src/components/Overview/Carousel/ExpandedView.jsx
+++ src/components/Overview/Carousel/ExpandedView.jsx
@@ -1,14 +1,40 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/void-dom-elements-no-children */
+/* eslint-disable arrow-body-style */
+/* eslint-disable semi */
 import React from 'react';
 import CarouselDefaultView from './CarouselDefaultView.jsx';
 
-const ExpandedView = (props) => {
-  return (
-    <>
-      {/* <div className="overview-carousel-expanded"></div> */}
-      <img className="overview-carousel-expanded" src={props.currentStyle.photos[props.currentImgIndex].url}></img>
-      <button className="close-button" onClick={(e) => props.changeView('default')}>close[x]</button>
-    </>
-  );
+class ExpandedView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      clicked: true,
+    });
+  }
+
+  render() {
+    return (
+      <div className="overview-carousel-expanded">
+        <img
+          className={this.state.clicked === true ? "carousel-expanded-img-zoom" : "carousel-expanded-img"}
+          src={this.props.currentStyle.photos[this.props.currentImgIndex].url}
+          onClick={this.handleClick}
+        />
+        <button className="close-button" onClick={(e) => this.props.changeView('default')}>close[x]</button>
+      </div>
+    );
+  }
 }
 
 export default ExpandedView;
