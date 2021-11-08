4fedfb7b812751825637bda01368d917e7f87995
Anna Yan <annayan102@gmail.com>
2021-11-08 15:13:42 -0500

    Work on pan functionality of expanded view

--- src/components/Overview/Carousel/ExpandedView.jsx
+++ src/components/Overview/Carousel/ExpandedView.jsx
@@ -6,15 +6,18 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable semi */
 import React from 'react';
-import CarouselDefaultView from './CarouselDefaultView.jsx';
 
 class ExpandedView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       clicked: false,
+      x: 0,
+      y: 0,
     };
+    this.zoom = React.createRef();
     this.handleClick = this.handleClick.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
   handleClick() {
@@ -23,13 +26,27 @@ class ExpandedView extends React.Component {
     });
   }
 
+  handleMouseMove(e) {
+    // const zoom = document.getElementsByClassName('carousel-expanded-img-zoom');
+    // console.log(e.clientX, e.clientY);
+    this.zoom.current.style.backgroundPositionX = `${-e.offsetX}px`;
+    this.zoom.current.style.backgroundPositionY = `${-e.offsetY}px`;
+    // this.zoom.current.style.transform = `translate(${e.offsetX}px, ${e.offsetY}px)`;
+  }
+
   render() {
     return (
       <div className="overview-carousel-expanded">
+          {/* style={{backgroundImage: this.props.currentImgIndex.url}}
+          ref={this.zoom}
+          onClick={this.handleClick}
+          onMouseMove={this.state.clicked === true ? (e) => this.handleMouseMove(e) : null} */}
         <img
-          className={this.state.clicked === true ? "carousel-expanded-img-zoom" : "carousel-expanded-img"}
+          ref={this.zoom}
+          className={this.state.clicked === true ? 'carousel-expanded-img-zoom' : 'carousel-expanded-img'}
           src={this.props.currentStyle.photos[this.props.currentImgIndex].url}
           onClick={this.handleClick}
+          onMouseMove={this.state.clicked === true ? (e) => this.handleMouseMove(e) : null}
         />
         <button className="close-button" onClick={(e) => this.props.changeView('default')}>close[x]</button>
       </div>
