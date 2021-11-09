f841456ab61747641fac3a4e7b3f2e8782ec2a1d
Anna Yan <annayan102@gmail.com>
2021-11-09 15:21:45 -0500

    Add basic grid styling

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -153,7 +153,7 @@ class Overview extends React.Component {
     }
 
     return (
-      <div className="overview-container">
+      <div className="overview-grid">
         <div className="product-info">
           <StarRatingStatic rating={this.state.avgRating/5} />
           <a>Read all reviews</a>
