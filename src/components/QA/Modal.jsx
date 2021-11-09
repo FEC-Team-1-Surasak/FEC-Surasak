7d31baa53d0acb1fc8e63f46b49b6a5547b102af
Xiaohan Liu <danae0730@gmail.com>
2021-11-08 19:21:33 -0800

    remove consle logs

--- src/components/QA/Modal.jsx
+++ src/components/QA/Modal.jsx
@@ -10,6 +10,9 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import axios from 'axios';
+import Body from './Body.jsx';
+import Nickname from './Nickname.jsx';
+import Email from './Email.jsx';
 
 class Modal extends React.Component {
   constructor(props) {
@@ -19,14 +22,14 @@ class Modal extends React.Component {
       nickname: '',
       email: '',
     };
-    this.questionChange = this.questionChange.bind(this);
+    this.labelChange = this.labelChange.bind(this);
     this.nameChange = this.nameChange.bind(this);
     this.emailChange = this.emailChange.bind(this);
     this.submit = this.submit.bind(this);
     this.isValid = this.isValid.bind(this);
   }
 
-  questionChange(e) {
+  labelChange(e) {
     this.setState({ question: e.target.value });
   }
 
@@ -43,13 +46,11 @@ class Modal extends React.Component {
     if (this.state.question === '' || this.state.name === '' || this.state.email === '') {
       return false;
     }
-
     if (this.state.email !== '') {
       console.log(this.state.email);
       const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       return re.test(this.state.email);
     }
-
     return true;
   }
 
@@ -81,24 +82,11 @@ class Modal extends React.Component {
             {' '}
             {this.props.productname}
           </h3>
-          <div className="your-question">
-            <h3>Your Question</h3>
-            <input onChange={this.questionChange} maxLength="1000" />
-          </div>
-
-          <div className="your-nickname">
-            <h3>What is your nickname?</h3>
-            <input onChange={this.nameChange} maxLength="60" placeholder="Example:jackson111" />
-            <h5><i>For privacy reasons, do not use your full name or email address</i></h5>
-          </div>
-
-          <div className="your-email">
-            <h3>Your email</h3>
-            <input onChange={this.emailChange} maxLength="60" placeholder="Why did you like the product or not?" />
-            <h5><i>For authentication reasons, you will not be emailed</i></h5>
-          </div>
-
+          <Body label="question" labelChange={this.labelChange} />
+          <Nickname nameChange={this.nameChange} />
+          <Email emailChange={this.emailChange} />
         </div>
+
         <div className="button">
           <button onClick={this.props.close}>Close</button>
           <button onClick={this.submit}>Submit Question</button>
--- src/components/QA/PhotoSection.jsx
+++ src/components/QA/PhotoSection.jsx
@@ -13,7 +13,6 @@ class PhotoSection extends React.Component {
   expand() {
     const curr = this.state.expand;
     this.setState({ expand: !curr });
-    console.log(this.state.expand);
   }
 
   render() {
@@ -21,13 +20,11 @@ class PhotoSection extends React.Component {
       <div className="display-photo">
         {this.props.photolist.map((photo) => (
           <div className="photo-container">
-            {console.log(photo)}
             <img className="photo-image" src={photo} alt="" width="100" height="150" onClick={this.expand.bind(this)} />
             <span className="close-icon" onClick={this.props.delete.bind(this)}>X</span>
             {this.state.expand
               ? (
                 <img className="expand-image" src={photo} alt="" onClick={this.expand.bind(this)} />
-
               )
               : null}
           </div>
