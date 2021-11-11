c0aa0b95126832f0fb5f6301ef02999b1f579886
Xiaohan Liu <danae0730@gmail.com>
2021-11-10 20:52:01 -0800

    update class name

--- src/components/QA/expandView.jsx
+++ src/components/QA/expandView.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const ExpandView = (props) => (
   <div className="expandlist">
     <div className="expand-photo" role="listbox">
-     <img src={props.url} alt="" onClick={props.click} width="400" height="350" />
+     <img src={props.url} alt="" onClick={props.click} width="400" height="350"/>
     </div>
     <div className="expand-photo-question">
-      <b>Q:{props.question}</b>
-      <div>A:{props.answer}</div>
+      <b>Question:{props.question}</b>
+       {' '}
+      <div className="expand-photo-answer">Answer:{props.answer}</div>
     </div>
 
   </div>
